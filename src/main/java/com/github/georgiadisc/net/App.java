150d67ea7c89fd65643fb00b84105eb4c4317ab7
georgiadisc <92945734+georgiadisc@users.noreply.github.com>
2023-02-12 00:38:11 +0200

    Renamed comms to catalogue

--- src/main/java/com/github/georgiadisc/net/App.java
+++ src/main/java/com/github/georgiadisc/net/App.java
@@ -27,29 +27,24 @@ public class App {
         Suspect s4 = new Suspect("John Papas", "Quick knife", "Greece", "Athens");
         s4.addNumber("0030210567888");
 
-        Communication[] comms = new Communication[15];
+        Communication[] catalogue = new Communication[15];
 
-        comms[0] = new PhoneCall("00496955444444", "00478484777777", 15, 10, 2022, 127);
-        comms[1] = new PhoneCall("00496955444444", "00478484777777", 16, 10, 2022, 240);
-        comms[2] = new PhoneCall("00446999888888", "00496955333333", 17, 10, 2022, 52);
-        comms[3] = new PhoneCall("00446999888888", "00478484777777", 18, 10, 2022, 180);
-        comms[4] = new PhoneCall("00478484666666", "00496955333333", 19, 10, 2022, 305);
-        comms[5] = new PhoneCall("00496955444444", "00478484222222", 20, 10, 2022, 247);
-        comms[6] = new PhoneCall("00478484222222", "00496955333333", 21, 10, 2022, 32);
+        catalogue[0] = new PhoneCall("00496955444444", "00478484777777", 15, 10, 2022, 127);
+        catalogue[1] = new PhoneCall("00496955444444", "00478484777777", 16, 10, 2022, 240);
+        catalogue[2] = new PhoneCall("00446999888888", "00496955333333", 17, 10, 2022, 52);
+        catalogue[3] = new PhoneCall("00446999888888", "00478484777777", 18, 10, 2022, 180);
+        catalogue[4] = new PhoneCall("00478484666666", "00496955333333", 19, 10, 2022, 305);
+        catalogue[5] = new PhoneCall("00496955444444", "00478484222222", 20, 10, 2022, 247);
+        catalogue[6] = new PhoneCall("00478484222222", "00496955333333", 21, 10, 2022, 32);
 
-        comms[7] =
-                new SMS("00496955444444", "00478484777777", 10, 10, 2022, "fancy a drink tonight?");
-        comms[8] = new SMS("00496955333333", "00446999888888", 11, 10, 2022, "Nitro Bomb prepared");
-        comms[9] = new SMS("00446999888888", "00496955444444", 12, 10, 2022,
-                "flying to Berlin tomorrow");
-        comms[10] = new SMS("00478484777777", "00446999888888", 13, 10, 2022,
-                "No internet connection today");
-        comms[11] = new SMS("00478484777777", "00446999888888", 14, 10, 2022,
-                "Gun Received from Rusty Knife");
-        comms[12] = new SMS("00478484777777", "00446999888888", 15, 10, 2022, "Metro Attack ready");
-        comms[13] = new SMS("00478484666666", "00446999888888", 16, 10, 2022,
-                "Explosives downtown have been placed");
-        comms[14] = new SMS("0030210567888", "00478484222222", 22, 10, 2022, "Meet you at Oslo");
+        catalogue[7] = new SMS("00496955444444", "00478484777777", 10, 10, 2022, "fancy a drink tonight?");
+        catalogue[8] = new SMS("00496955333333", "00446999888888", 11, 10, 2022, "Nitro Bomb prepared");
+        catalogue[9] = new SMS("00446999888888", "00496955444444", 12, 10, 2022, "flying to Berlin tomorrow");
+        catalogue[10] = new SMS("00478484777777", "00446999888888", 13, 10, 2022, "No internet connection today");
+        catalogue[11] = new SMS("00478484777777", "00446999888888", 14, 10, 2022, "Gun Received from Rusty Knife");
+        catalogue[12] = new SMS("00478484777777", "00446999888888", 15, 10, 2022, "Metro Attack ready");
+        catalogue[13] = new SMS("00478484666666", "00446999888888", 16, 10, 2022, "Explosives downtown have been placed");
+        catalogue[14] = new SMS("0030210567888", "00478484222222", 22, 10, 2022, "Meet you at Oslo");
 
         Registry registry = new Registry();
 
@@ -59,7 +54,7 @@ public class App {
         registry.addSuspect(s4);
 
         for (int i = 0; i < 15; i++) {
-            registry.addCommunication(comms[i]);
+            registry.addCommunication(catalogue[i]);
         }
 
         registry.buildGraph();
